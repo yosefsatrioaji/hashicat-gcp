783668062ad2bc82e4b00e1229836803f6d4383b
Yosef Satrio Aji <yosef260501@gmail.com>
2023-08-27 23:57:17 +0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,8 @@ module "network" {
   source  = "app.terraform.io/YSA26_TEST/network/google"
   version = "3.4.0"
   # insert required variables here
+network_name="gaurav-network"
+project_id=var.project
 subnets = [
   {
     subnet_name   = "gaurav-subnet"
